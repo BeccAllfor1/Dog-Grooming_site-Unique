e077c2654ab0cfee6fc3abd1fa8a1e8e1fc3576b
Rebecca Allford <rebeccaallford1@gmail.com>
2024-04-14 19:35:09 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// let groomingSalonName ="Unique Grooms"
+// // console.log (${groomingSalonName});
+
+// const cute_dog = "cutedog.jpg"
+// const funny_dog = "funnydog.jpg"
+// let showing_cute = true
+
+// const change_picture = () => {
+//     showing_cute = !showing_cute
+//     const picture = document.getElementById("doggy")
+//     if (showing_cute) {
+//         picture.src = cute_dog
+//     } else {
+//         picture.src = funny_dog
+//     }
+// }
+
+// change_picture()
+// change_picture()
+// change_picture()
+// change_picture()
+
+// setInterval(change_picture, 1000)
+
+// const add2 = (x) => {
+//     return x + 2
+// }
+
+// console.log(add2(5))
+// console.log(add2(6))
+// console.log(add2(7))
+// console.log(add2(11))
+
+    
